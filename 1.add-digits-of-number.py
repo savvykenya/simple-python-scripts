849d876e99703bacc47fe9424e0d140659582742
Harriet Ocharo <60106240+savvykenya@users.noreply.github.com>
2020-04-26 18:18:00 +0900

    Rename

--- 1.add-digits-of-number.py
+++ 1.add-digits-of-number.py
@@ -1,6 +1,6 @@
 #Given a number, add its digits.
 #e.g. given 29  then 2+9 =11 or 239 then return 2+3+9
-#Python has a fast and easy solution. In other languages, you may have to use % function.
+#Python has a fast and easy solution. In other approaches, you can use / and % functions.
 #convert to string, use list comprehension to make it a list of integers, use inbuilt sum() function
 
 def addTwoDigits(n):
